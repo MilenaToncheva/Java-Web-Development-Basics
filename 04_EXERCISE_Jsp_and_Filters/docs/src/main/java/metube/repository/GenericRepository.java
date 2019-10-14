e28ef77a1cf90824d3e54b7f4855b2ca3a7874ae
Milena Toncheva <39679176+MilenaToncheva@users.noreply.github.com>
2019-10-14 16:28:16 +0300

    Create GenericRepository.java

---
+++ 04_EXERCISE_Jsp_and_Filters/docs/src/main/java/metube/repository/GenericRepository.java
@@ -0,0 +1,10 @@
+package metube.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface GenericRepository<E,K> {
+    E save(E entity);
+    List<E> findAll();
+    Optional findById(K id);
+}
