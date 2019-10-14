e8c8b449704ce27c0e8b1ac0afb117f85148054e
Milena Toncheva <39679176+MilenaToncheva@users.noreply.github.com>
2019-10-14 16:30:47 +0300

    Create TubeService.java

---
+++ 04_EXERCISE_Jsp_and_Filters/docs/src/main/java/metube/service/TubeService.java
@@ -0,0 +1,12 @@
+package metube.service;
+
+import metube.domain.models.service.TubeServiceModel;
+
+import java.util.List;
+
+public interface TubeService {
+    void saveTube(TubeServiceModel tubeServiceModel);
+
+    TubeServiceModel findByName(String name);
+    List<TubeServiceModel> findAll();
+}
