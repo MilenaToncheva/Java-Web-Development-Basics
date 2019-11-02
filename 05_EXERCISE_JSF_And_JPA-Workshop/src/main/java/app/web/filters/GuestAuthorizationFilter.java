18f60b9bd431b436fe26d85744f318ea726e8119
Milena Toncheva <39679176+MilenaToncheva@users.noreply.github.com>
2019-11-02 19:31:27 +0200

    Create GuestAuthorizationFilter.java

---
+++ 05_EXERCISE_JSF_And_JPA-Workshop/src/main/java/app/web/filters/GuestAuthorizationFilter.java
@@ -0,0 +1,23 @@
+package app.web.filters;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter({"/view/home.xhtml","/view/add-job.xhtml","/view/logout.xhtml"})
+public class GuestAuthorizationFilter implements Filter{
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req= (HttpServletRequest) servletRequest;
+        HttpServletResponse resp= (HttpServletResponse) servletResponse;
+        HttpSession session=req.getSession();
+        if(session.getAttribute("userId")==null){
+            resp.sendRedirect("/view/index.xhtml");
+            return;
+        }
+        filterChain.doFilter(req,resp);
+    }
+}
