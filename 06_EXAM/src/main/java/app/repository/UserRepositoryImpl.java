4c25b2c5a773eb621205959b15a8590e90699100
Milena Toncheva <39679176+MilenaToncheva@users.noreply.github.com>
2019-11-07 15:09:04 +0200

    Create UserRepositoryImpl.java

---
+++ 06_EXAM/src/main/java/app/repository/UserRepositoryImpl.java
@@ -0,0 +1,58 @@
+package app.repository;
+
+import app.domain.entities.User;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import java.util.List;
+
+public class UserRepositoryImpl implements UserRepository {
+    private final EntityManager entityManager;
+
+    @Inject
+    public UserRepositoryImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public User findByUsernameAndPassword(String username, String password) {
+
+        return this.entityManager.createQuery("SELECT u FROM User u WHERE u.username=:username AND u.password=:password", User.class)
+                .setParameter("username", username).setParameter("password", password)
+                .getSingleResult();
+    }
+
+    @Override
+    public void save(User user) {
+        this.entityManager.getTransaction().begin();
+        this.entityManager.persist(user);
+        this.entityManager.getTransaction().commit();
+    }
+
+    @Override
+    public User findById(String id) {
+        return this.entityManager.createQuery("SELECT u FROM User u WHERE u.id=:id", User.class)
+                .setParameter("id", id)
+                .getSingleResult();
+    }
+
+    @Override
+    public List<User> findAll() {
+        return this.entityManager.createQuery("SELECT u FROM User u", User.class).getResultList();
+    }
+
+    @Override
+    public void update(User user) {
+        this.entityManager.getTransaction().begin();
+        this.entityManager.merge(user);
+        this.entityManager.getTransaction().commit();
+    }
+
+    @Override
+    public void delete(String id) {
+        this.entityManager.getTransaction().begin();
+        User user = this.entityManager.find(User.class, id);
+        this.entityManager.remove(user);
+        this.entityManager.getTransaction().commit();
+    }
+}
